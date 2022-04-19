99f9416d35e6da3b87ee93bcbd915ba3f622e376
@DanielMora10 <djkevindaniel2@gmail.com>
2022-04-19 09:22:31 -0500

    Proyecto Terminado

--- app/src/main/java/com/example/proyectobluetooh/Actividades.java
+++ app/src/main/java/com/example/proyectobluetooh/Actividades.java
@@ -2,21 +2,61 @@ package com.example.proyectobluetooh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Actividades extends AppCompatActivity {
+    private String[] placasVehiculos;
 
     ImageView parquaderoactivity, iconoInformacion;
     TextView placaVehiculoET,nombreUsuarioET;
+    EditText lugarparkeadero;
+    private RequestQueue requestQueue;
+    String ip ="192.168.190.237";
+    String placaIngresada ="";
+    String nombreConductor="";
+    String lugarParqueadero="";
+    String address;
+    private String urlDriveraddActivo="http://"+ip+":4000/clientes";
+    //http://"+"ip+"+":4000/clientes";
 
 
     //String que se enviará a actividad principal, mainactivity
     public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    private Request<? extends Object> stringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +67,12 @@ public class Actividades extends AppCompatActivity {
         iconoInformacion = findViewById(R.id.iconoInformacion);
         placaVehiculoET = findViewById(R.id.placaVehiculoET);
         nombreUsuarioET = findViewById(R.id.nombreUsuarioET);
+        lugarparkeadero=findViewById(R.id.lugarParqueadero);
 
 
         Bundle extras = getIntent().getExtras();
-        String address = extras.getString(EXTRA_DEVICE_ADDRESS);
+         address = extras.getString(EXTRA_DEVICE_ADDRESS);
+        placasVehiculos=getResources().getStringArray(R.array.arrayPlacas);
 
 
 
@@ -40,28 +82,101 @@ public class Actividades extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                String placaIngresada= placaVehiculoET.getText().toString();
-                String nombreConductor= nombreUsuarioET.getText().toString();
-                if(placaIngresada.isEmpty() || nombreConductor.isEmpty()){
-                    Toast.makeText(getBaseContext(), "Ingrese Datos" + "Nombre - Placa", Toast.LENGTH_SHORT).show();
-                } else{
-                    Toast.makeText(getBaseContext(), "Abrio ACTIVITY" + address, Toast.LENGTH_SHORT).show();
+                 placaIngresada= placaVehiculoET.getText().toString();
+                 nombreConductor= nombreUsuarioET.getText().toString();
+                 lugarParqueadero=lugarparkeadero.getText().toString();
+                if(placaIngresada.isEmpty() || nombreConductor.isEmpty() || lugarParqueadero.isEmpty()){
+                    Toast.makeText(getBaseContext(), "Ingrese Datos" + "Nombre - Placa - Numero Parqueadero", Toast.LENGTH_SHORT).show();
+                } else {
+
+                   //onCreateDialogPlacas();
+
                     Intent intent = new Intent(Actividades.this, MainActivity.class);
                     intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+                    enviarDatosPakingActivos();
                     startActivity(intent);
+
+//                    Toast.makeText(getBaseContext(), "Abrio ACTIVITY" + address, Toast.LENGTH_SHORT).show();
+                   //
                 }
 
             }
         });
 
-//        onff.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Actividades.this, ApagadoEncendido.class);
-//                intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
-//                startActivity(intent);
-//            }
-//        });
 
     }
+
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+    public void enviarDatosPakingActivos(){
+        //Toast.makeText(Actividades.this,"Heyy >>"+placa+" : "+name,Toast.LENGTH_SHORT).show();
+        Toast.makeText(Actividades.this,"Heyy >>",Toast.LENGTH_SHORT).show();
+
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, urlDriveraddActivo, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                ////////////
+                ///////////  Respusta del servidor///////////
+                ////////////
+                Toast.makeText(Actividades.this,"[]"+response,Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Log.e("Error Volelly",error.getMessage());
+            }
+        }){
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<>();
+                params.put("placa",placaIngresada);
+                params.put("nombre",nombreConductor);
+                params.put("lugar",lugarParqueadero);
+                return params;
+            }
+        };
+        postRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 2, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        Volley.newRequestQueue(this).add(postRequest);
+
+
+    }
+
+
+
+
+
+
+//    public void onCreateDialogPlacas() {
+//        TimePickerDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle(R.string.placas)
+//                .setItems(R.array.arrayPlacas, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        //Toast.makeText(Actividades.this,"Selecionaste :"+placasVehiculos[which],Toast.LENGTH_SHORT).show();
+//
+//
+//
+//                    }
+//                });
+//        Dialog dialog= builder.create();
+//        dialog.show();
+//    }
+
 }
--- app/src/main/java/com/example/proyectobluetooh/MainActivity.java
+++ app/src/main/java/com/example/proyectobluetooh/MainActivity.java
@@ -3,21 +3,38 @@ package com.example.proyectobluetooh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.renderscript.ScriptGroup;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,8 +43,8 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnEncenderTodoLuces,btnApagarTodoLuces,btnSitio1,btnSitio2,btnSitio3,btnSitio4;
-    private ImageButton imageViewPersona;
+    private Button btnEncenderTodoLuces,btnApagarTodoLuces,btnSitio1,btnSitio2,btnSitio3,btnSitio4,btnSitio5,listar,cobrar;
+    private TextView lugarparkeadero;
     Handler bluetoothIn;
     final int handlerState = 0;
     private BluetoothAdapter btAdapter = null;
@@ -38,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
     private static final UUID BITMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     //String para la dirección MAC
     private static String address = null;
+    String ip ="192.168.190.237";
+    private String url="http://"+ip+":4000/cuentas";
+    private RequestQueue requestQueue;
+    private boolean presionado = false;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -62,6 +83,10 @@ public class MainActivity extends AppCompatActivity {
         btnSitio2 = findViewById(R.id.btnSitio2);
         btnSitio3 = findViewById(R.id.btnSitio3);
         btnSitio4 = findViewById(R.id.btnSitio4);
+        btnSitio5 = findViewById(R.id.btnSitio5);
+        cobrar = findViewById(R.id.cobrar);
+        lugarparkeadero=findViewById(R.id.lugarparkeadero);
+
         //imageViewPersona = findViewById(R.id.imageViewPersona);
 
 
@@ -80,45 +105,56 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnSitio1.setOnClickListener(v -> {
+            MyConexionBT.write("6");
+        });
+
+//        btnSitio1.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                        if (!presionado) {
+//                            presionado = true;
+//                            //AsyncTask que ejecuta Tarea.
+//                            MyConexionBT.write("a");
+//                        }
+//                        break;
+//                    case MotionEvent.ACTION_UP:
+//                        presionado = false;
+//                        Log.d("CONTADOR", "Detiene contador");
+//                        break;
+//                }
+//                return true;
+//            }
+//
+//        });
+
+        btnSitio2.setOnClickListener(v -> {
             MyConexionBT.write("2");
         });
 
-        btnSitio2.setOnClickListener(v -> {
+        btnSitio3.setOnClickListener(v -> {
             MyConexionBT.write("3");
         });
 
-        btnSitio3.setOnClickListener(v -> {
+        btnSitio4.setOnClickListener(v -> {
             MyConexionBT.write("4");
         });
 
-        btnSitio4.setOnClickListener(v -> {
+        btnSitio5.setOnClickListener(v -> {
             MyConexionBT.write("5");
         });
 
-//        imageViewPersona.setOnClickListener(v -> {
-//            MyConexionBT.write("7");
-//        });
+        cobrar.setOnClickListener(v -> {
+
+            MyConexionBT.write("z");
+            //onCreateDialogCobrar(2);
+            //traerSolicitudes();
+
+        });
 
 
 
-    }
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideSystemUI();
-        }
-    }
-
-    private void hideSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device)throws IOException{
@@ -219,5 +255,88 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+
+
+    public  void traerSolicitudes(){
+        StringRequest request=new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        JSONObject jsonObject = null;
+                        try {
+                            jsonObject = new JSONObject(response);
+                            int idClientes=jsonObject.getInt("idClientes");
+                            String placa=jsonObject.getString("placa").toString();
+                            String nombre=jsonObject.getString("nombre").toString();
+                            String lugar=jsonObject.getString("lugar").toString();
+                            String horaIngreso=jsonObject.getString("horaIngreso").toString();
+                            //Toast.makeText(Inicio.this,"-> "+startDireccion,Toast.LENGTH_SHORT).show();
+                            // Toast.makeText(Inicio.this,"-> "+jsonObject.getString("FinalDirection").toString(),Toast.LENGTH_SHORT).show();
+                            onCreateDialogCobrar(idClientes);
+                            //int idRequest,Double latitud,Double longitud,String nameUser,String referencia
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+               // Log.e("Error Volelly",error.getMessage());
+            }
+        }
+        );
+        Volley.newRequestQueue(this).add(request);
+    }
+    public void onCreateDialogCobrar(int id2) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Get the layout inflater
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_signin, null))
+                // Add action buttons
+                .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        lugarparkeadero.setText(id2);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        Dialog dialog= builder.create();
+           dialog.show();
     }
 }
